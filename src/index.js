195d132b41513a3edeaa5ef42f0393fc7bebf5ad
1evanter <rita.oleksenkoo@gmail.com>
2024-10-12 13:29:41 +0300

    start GlobalStyles

--- src/index.js
+++ src/index.js
@@ -5,12 +5,14 @@ import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
 import { theme } from 'styles/theme';
+import { GlobalStyles } from 'styles/globalStyles';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter basename="/anhelina-portfolio-new">
       <ThemeProvider theme={theme}>
         <App />
+        <GlobalStyles />
       </ThemeProvider>
     </BrowserRouter>
   </React.StrictMode>
